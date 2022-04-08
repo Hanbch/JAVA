4ddb0a03555365c4f9bb1c695ac795f872791b9d
unknown <hhbbii@nate.com>
2022-04-08 18:39:14 +0900

    04/8

---
+++ 3day/UserVariable/src/UserVariable.java
@@ -0,0 +1,13 @@
+
+public class UserVariable {
+
+	public static void main(String[] args) {
+		short num1 = 10;			
+		short num2= 20; 		
+		
+		int num3 = num1 + num2; 
+		
+		System.out.println(num1 + " + " + num2 + " = " + num3 );
+	}
+
+}
