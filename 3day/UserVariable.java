de7bab62ed7a2988e52629cf59cd2877d071fbf9
Hanbchoi <hhbbii@nate.com>
2022-04-11 22:31:46 +0900

    0408

--- 3day/UserVariable.java
+++ 3day/UserVariable.java
@@ -2,12 +2,13 @@
 public class UserVariable {
 
 	public static void main(String[] args) {
+		
 		short num1 = 10;			
 		short num2= 20; 		
 		
-		int num3 = num1 + num2; 
+		int num3 = num1 *  num2; 
 		
-		System.out.println(num1 + " + " + num2 + " = " + num3 );
+		System.out.println(num1 + " X " + num2 + " = " + num3 );
 	}
 
 }
