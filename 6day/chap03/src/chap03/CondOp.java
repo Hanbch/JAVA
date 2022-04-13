93cfc0a5fc09b4badd8d1fa3889d074ff8eefd34
unknown <hhbbii@nate.com>
2022-04-13 18:21:54 +0900

    0413

---
+++ 6day/chap03/src/chap03/CondOp.java
@@ -0,0 +1,22 @@
+package chap03;
+
+public class CondOp {
+
+	public static void main(String[] args) {
+		
+		// 삼항연산자
+		
+		int num1 = 50;
+		int num2 = 100;
+		int big;
+		int diff;
+		
+		big = ( num1 > num2 ) ? num1 : num2; // ( 괄호 )안의 조건식이 참이면 ? 앞에 값이 리턴됨 거짓이면  뒤에값이 리턴됨
+		System.out.println("큰 수: " + big);
+		
+		diff = (num1 > num2) ? (num1 - num2) : (num2 - num1);
+		System.out.println("절댓값: " + diff );
+		
+	}
+
+}
