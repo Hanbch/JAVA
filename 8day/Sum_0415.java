67af5c0d8409d78c838411a9bbbe0e834e8ce08f
Hanbchoi <hhbbii@nate.com>
2022-04-15 19:27:02 +0900

    0415

---
+++ 8day/Sum_0415.java
@@ -0,0 +1,83 @@
+package chap05;
+
+public class Sum_0415 {
+
+	public static void main(String[] args) {
+		//1.아래의 Star를 찍으시오.
+		//*****
+		//*****
+		//*****
+		//*****
+		//*****
+		for(int i=0; i<5; i++) {
+			for(int j=0; j<=5; j++) {
+				System.out.print("*");
+			}
+			System.out.print('\n');
+		}
+		
+		System.out.print('\n');
+		//*
+		//**
+		//***
+		//****
+		//*****
+		for(int i=0; i<5; i++) {
+			for(int j=0; j<=i; j++) {
+				System.out.print("*");
+			}
+			System.out.print('\n');
+		}
+		System.out.print('\n');
+		
+		//*****
+		//****
+		//***
+		//**
+		//*		
+		
+		for(int i=0; i<5; i++) {
+			for(int j=0; j<5-i; j++) {
+				System.out.print("*");
+			}
+			System.out.print('\n');
+		}
+		System.out.print('\n');
+		
+		//    *
+		//   **
+		//  ***
+		// ****
+		//*****		
+		
+		for(int i=0; i<5; i++) {
+			for(int j=0; j<4-i; j++) {
+				System.out.print(" ");
+			}
+			for(int j=0; j<=i; j++) {
+				System.out.print("*");
+			}
+			System.out.print('\n');
+		}
+		System.out.print('\n');
+		
+		//    *
+		//   ***
+		//  *****
+		// *******
+		//*********
+		
+		for(int i=0; i<5; i++) {
+			for(int j=0; j<4-i; j++) {
+				System.out.print(" ");
+			}
+			for(int j=0; j<(i*2)+1; j++) {
+				System.out.print("*");
+			}
+			System.out.print('\n');
+		}
+		System.out.print('\n');
+		
+	}
+
+}
