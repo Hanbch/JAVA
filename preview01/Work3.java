7d6a8dc5d1ab8dbca199c4fcf30b6e889b430b40
Hanbchoi <hhbbii@nate.com>
2022-04-17 11:30:12 +0900

    0417

---
+++ preview01/Work3.java
@@ -0,0 +1,62 @@
+package practice01;
+import java.util.Scanner;
+
+class FarmMachine {
+	//속성 attribute
+	int price;
+	int year;
+	String color;
+	
+	//동작
+	void move(int amount) {
+		System.out.println("Machine is moving" + amount);	
+	}
+	
+	void dig(int amount) {
+		System.out.println("Machine id digging" + amount);
+	}
+	
+}
+class InterAge {
+	void calInterAge(int born_date,int today_date){
+		System.out.println(today_date - born_date);
+	}
+}
+
+public class Work3 {
+	
+	
+	public static void main(String[] args) {
+	FarmMachine fm = new FarmMachine();
+	
+	fm.price = 100;
+	fm.year = 2022;
+	fm.color = "red";
+		  
+	fm.move(50);
+	fm.dig(100);
+	
+	System.out.println(fm.price);
+	System.out.println(fm.year);
+	System.out.println(fm.color);
+
+	
+	//나이 계산
+
+	int born_date;
+	int today_date;
+	
+	Scanner sc = new Scanner(System.in);
+	InterAge ia = new InterAge();
+	
+	System.out.println("태어난 연도4자리를 입력하시오. ex)1995");
+	born_date = sc.nextInt();
+	
+	System.out.println("현재 년도4자리를 입력하시오");
+	today_date = sc.nextInt();
+	
+	ia.calInterAge(born_date,today_date);
+	
+	
+	}
+}
