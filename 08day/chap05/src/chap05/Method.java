b596f84963aafea9681bf5f63100242a9a5813a1
HanBe Choi <97681201+Hanbch@users.noreply.github.com>
2022-04-19 15:32:18 +0900

    04/15

--- 08day/chap05/src/chap05/Method.java
+++ 08day/chap05/src/chap05/Method.java
@@ -9,6 +9,7 @@ public class Method {
 		hiEveryone(13);
 		System.out.println("프로그램의 끝");
 	}
+	
 	public static void hiEveryone(int age) {
 		System.out.println("좋은 아침입니다.");
 		System.out.println("당신의 나이는"+ age +"입니다.");
